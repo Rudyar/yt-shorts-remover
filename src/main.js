52d7c15379aa2231fe4a6742594b26bf599e0cec
arudy <arudy@student.42.fr>
2023-03-23 12:09:03 +0100

    add sidebar + search + change url

--- src/main.js
+++ src/main.js
@@ -1,50 +1,97 @@
 let uri = location.href;
 
-function delShortsSubTab() {
+function delShortsSideBar() {
+  let shortsIcon = document.querySelectorAll("#endpoint[title='Shorts'");
+
+  if (shortsIcon.length == 0) {
+    setTimeout(() => {
+      delShortsSideBar();
+    }, 500);
+  }
+
+  shortsIcon.forEach((e) => {
+    e.style.display = "none";
+  });
+}
+
+function delShortsSub() {
   let elems = document.querySelectorAll(
     "ytd-grid-video-renderer, ytd-video-renderer"
   );
+
   if (elems.length == 0) {
     setTimeout(() => {
-      delShortsSubTab();
+      delShortsSub();
     }, 500);
   }
+
   elems.forEach((e) => {
     if (e.querySelector("a").href.includes("/shorts/")) {
-      e.remove();
+      e.style.display = "none";
     }
   });
 }
 
 function delShortsHome() {
-  logConsole("DelHome");
   let elems = document.querySelectorAll(".ytd-rich-shelf-renderer");
+
   if (elems.length == 0) {
     setTimeout(() => {
       delShortsHome();
     }, 500);
   }
-  logConsole(elems);
+
   elems.forEach((e) => {
     if (e.querySelector("h2")?.textContent.includes("Shorts")) {
-      e.remove();
+      e.style.display = "none";
     }
   });
 }
 
-function waitDOMElem() {
-  if (document.body != null) {
-    if (uri === "https://www.youtube.com/feed/subscriptions") {
-      delShortsSubTab();
-    } else if (uri === "https://www.youtube.com/") {
+function delShortsSearch() {
+  let shelf = document.querySelectorAll("#contents > ytd-reel-shelf-renderer");
+  let shorts = document.querySelectorAll("ytd-video-renderer");
+
+  if (shelf.length == 0) {
+    setTimeout(() => {
       delShortsHome();
-    } else {
-      logConsole(uri);
+    }, 500);
+  }
+
+  shelf.forEach((e) => {
+    e.style.display = "none";
+  });
+
+  shorts.forEach((e) => {
+    if (e.querySelector("a").href.includes("/shorts/")) {
+      e.style.display = "none";
     }
+  });
+}
+
+function dispatch() {
+  uri = location.href;
+  if (uri === "https://www.youtube.com/feed/subscriptions") {
+    delShortsSub();
+  } else if (uri === "https://www.youtube.com/") {
+    delShortsHome();
+  } else if (uri.includes("https://www.youtube.com/results?search_query=")) {
+    delShortsSearch();
+  }
+}
+
+function waitDOMElem() {
+  let body = document.body;
+
+  if (body != null) {
+    dispatch();
+    delShortsSideBar();
+    let obs = new MutationObserver(dispatch);
+    obs.observe(body, { subtree: true, childList: true });
   } else {
     setTimeout(() => {
       waitDOMElem();
-    }, 1000);
+    }, 500);
   }
 }
 
@@ -52,8 +99,8 @@ window.addEventListener("load", () => {
   waitDOMElem();
 });
 
-function logConsole(str) {
-  console.log("==================");
-  console.log(str);
-  console.log("==================");
-}
+// function logConsole(str) {
+//   console.log("==================");
+//   console.log(str);
+//   console.log("==================");
+// }
